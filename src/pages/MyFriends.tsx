bff8be78fb11f4a87c7c5b261fdc592a3d1abd96
gregroyclark <gregroyclark@gmail.com>
2023-12-17 23:26:48 -0800

    added component stub

---
+++ src/pages/MyFriends.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyFriends = () => {
+  return (
+    <div>
+      <div>My Friends</div>
+    </div>
+  );
+};
+
+export default MyFriends;
---
+++ src/pages/UpdateFriend.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateFriend = () => {
+  return (
+    <div>
+      <div>Update Friend</div>
+    </div>
+  );
+};
+
+export default UpdateFriend;
