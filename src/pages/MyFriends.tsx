4a971222284bdeee435e9cf00c42591d1d50aac3
gregroyclark <gregroyclark@gmail.com>
2023-12-18 03:24:40 -0800

    added MyFriends component

--- src/pages/MyFriends.tsx
+++ src/pages/MyFriends.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import React, { useState, useEffect } from "react";
+
+import Layout from "~/components/Layout";
+import { getFriends } from "prisma/prismaService";
 
 const MyFriends = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        const friendsData = await getFriends();
+        setFriends(friendsData);
+      } catch (error) {
+        console.error("Error fetching friends:", error);
+      }
+    };
+    void fetchFriends();
+  }, []);
+
   return (
-    <div>
-      <div>My Friends</div>
-    </div>
+    <Layout>
+      <div className="mx-auto mt-8 max-w-md bg-white p-4 shadow-md">
+        <h1 className="mb-4 flex justify-center text-lg font-semibold text-gray-600">
+          My Friends
+        </h1>
+        <hr className="mb-4" />
+        <table className="w-full border border-gray-300">
+          <thead>
+            <tr>
+              <th className="border-b p-2">First Name</th>
+              <th className="border-b p-2">Last Name</th>
+              <th className="border-b p-2">Phone Number</th>
+              <th className="border-b p-2">Email</th>
+              <th className="border-b p-2">Notes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {friends.map((friend) => (
+              <tr key={friend.id}>
+                <th className="border-b p-2">{friend.firstName}</th>
+                <th className="border-b p-2">{friend.lastName}</th>
+                <th className="border-b p-2">{friend.phoneNumber}</th>
+                <th className="border-b p-2">{friend.email}</th>
+                <th className="border-b p-2">{friend.notes}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
   );
 };
 
