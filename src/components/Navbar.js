7ae2a408ba9a7e5bc3a848f368197c314f203ece
gregroyclark <gregroyclark@gmail.com>
2023-12-17 23:26:19 -0800

    added component stub

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed z-[100] h-24 w-full bg-[#f8f8f8] shadow-lg">
+      <div className="flex h-full w-full items-center justify-between p-2 2xl:px-16">
+        MyFriendBook
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
