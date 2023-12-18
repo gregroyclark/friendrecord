6d2dff90d84776263902fa3897c4ad13f69f0e5c
gregroyclark <gregroyclark@gmail.com>
2023-12-18 00:11:16 -0800

    added API with post request

---
+++ src/pages/api/addFriend.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { createFriend } from "prisma/prismaService";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    try {
+      const { firstName, lastName, phoneNumber, email, notes } = req.body;
+      const newFriend = await createFriend({
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        notes,
+      });
+      res.status(201).json(newFriend);
+    } catch (error) {
+      console.error("Error adding friend:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
