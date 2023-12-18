d025e959fec1aabc966d13818f3bcc594f62d9c1
gregroyclark <gregroyclark@gmail.com>
2023-12-18 00:10:44 -0800

    added prismaService

---
+++ prisma/prismaService.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createFriend = async (data: {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  email: string;
+  notes: string;
+}) => {
+  try {
+    const newFriend = await prisma.friend.create({
+      data,
+    });
+    return newFriend;
+  } catch (error) {
+    console.error("Error creating friend:", error);
+    throw error;
+  }
+};
