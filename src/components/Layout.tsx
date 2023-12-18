75fa1f3115290935354f2d3d6aee1a9f875f510c
gregroyclark <gregroyclark@gmail.com>
2023-12-18 03:24:21 -0800

    fixed layout bugs

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,14 +4,20 @@ import Navbar from "./Navbar";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Layout = ({ children }: { children: ReactElement }) => {
   return (
-    <div>
-      <Navbar />
-      <main className="absolute left-0 top-0 flex min-h-screen w-full bg-white/10">
-        <div className="m-auto h-full w-full max-w-[700px] flex-col items-start justify-center">
-          {children}
-        </div>
-      </main>
-    </div>
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+    // <div>
+    //   <Navbar />
+    //   <main className="absolute left-0 top-0 flex min-h-screen w-full bg-white/10">
+    //     <div className="m-auto h-full w-full max-w-[700px] flex-col items-start justify-center">
+    //       {children}
+    //     </div>
+    //   </main>
+    // </div>
   );
 };
 
