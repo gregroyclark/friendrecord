7d164633ca9fe6dd1a4ab52af2ff237f6fc756e1
gregroyclark <gregroyclark@gmail.com>
2023-12-18 01:53:53 -0800

    updated styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,13 @@
 import React, { type ReactElement } from "react";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Layout = ({ children }: { children: ReactElement }) => {
   return (
     <div>
       <Navbar />
-      <main className="absolute left-0 top-0 h-screen w-full bg-white/10">
-        <div className="m-auto flex h-full w-full max-w-[700px] flex-col items-start justify-center">
+      <main className="absolute left-0 top-0 flex min-h-screen w-full bg-white/10">
+        <div className="m-auto h-full w-full max-w-[700px] flex-col items-start justify-center">
           {children}
         </div>
       </main>
