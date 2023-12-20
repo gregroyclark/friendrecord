2dd189f9a254a9035435f0771466fb90ef8c0a68
gregroyclark <gregroyclark@gmail.com>
2023-12-19 19:55:54 -0800

    added NextApiRequest & Response

--- src/pages/api/getFriend.ts
+++ src/pages/api/getFriend.ts
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+import { type NextApiRequest, type NextApiResponse } from "next";
+
 import { getFriend } from "prisma/prismaService";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   const { id } = req.query;
   try {
-    const friend = await getFriend(Number(id));
+    const friend = await getFriend(id);
     res.status(200).json(friend);
   } catch (error) {
     res.status(500).json({ error: "Error fetching friend" });
