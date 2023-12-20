1fc5aef1a3edab6a066bc0b6ec8a4550d78193ce
gregroyclark <gregroyclark@gmail.com>
2023-12-19 20:20:09 -0800

    added Head

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,17 @@
+import Head from "next/head";
 import React, { type ReactElement } from "react";
+
 import Navbar from "./Navbar";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Layout = ({ children }: { children: ReactElement }) => {
   return (
     <html lang="en">
+      <Head>
+        <title>MyFriendBook</title>
+        <meta name="description" content="Welcome to MyFriendBook" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="min-h-screen">
         <Navbar />
         <main className="m-8 flex h-screen items-center justify-center">
