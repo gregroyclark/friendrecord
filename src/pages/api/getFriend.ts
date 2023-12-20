2ef96d766b65a699013b1ba349efa23c9af9fea9
gregroyclark <gregroyclark@gmail.com>
2023-12-20 03:43:34 -0800

    added condition moved query into try block

--- src/pages/api/getFriend.ts
+++ src/pages/api/getFriend.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 import { getFriend } from "prisma/prismaService";
@@ -9,14 +6,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const { id } = req.query;
-  console.log("getFriend.ts id", id);
-  const friendId = Array.isArray(id) ? Number(id[0]) : Number(id);
-  try {
-    const friend = await getFriend(Number(friendId));
-    console.log("getFriend Friend: ", typeof friend, friend);
-    res.status(200).json(friend);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching friend" });
-  }
+  if (req.method === "GET")
+    try {
+      const { id } = req.query;
+      console.log("getFriend.ts id", id);
+
+      const friend = await getFriend(Number(id));
+      console.log("getFriend Friend: ", typeof friend, friend);
+      res.status(200).json(friend);
+    } catch (error) {
+      res.status(500).json({ error: "Error fetching friend" });
+    }
 }
