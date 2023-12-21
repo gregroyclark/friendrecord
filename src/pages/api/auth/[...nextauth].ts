11d9cfd475000633b403311f123a717d065cecb1
gregroyclark <gregroyclark@gmail.com>
2023-12-21 04:17:46 -0800

    ignore auth stuff for now

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-import NextAuth from "next-auth";
+// /* eslint-disable @typescript-eslint/no-unsafe-argument */
+// import NextAuth from "next-auth";
 
-import { authOptions } from "~/server/auth";
+// import { authOptions } from "~/server/auth";
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
