3ebc073d98ea098bf5b95d55eabbccf476389d4d
gregroyclark <gregroyclark@gmail.com>
2023-12-20 18:30:16 -0800

    added test suite for getAllFriends

--- prisma/prismaService.test.ts
+++ prisma/prismaService.test.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { describe } from "node:test";
+// import { expect, jest } from "@jest/globals";
+
 import {
   createFriend,
   getAllFriends,
@@ -6,6 +10,8 @@ import {
   updateFriend,
   deleteFriend,
 } from "./prismaService";
+
+import prisma from "./prisma";
 
 void describe("prismaService", () => {
   it("should create a friend", async () => {
@@ -18,5 +24,46 @@ void describe("prismaService", () => {
     };
     const newFriend = await createFriend(friendData);
     expect(newFriend).toEqual(expect.objectContaining(friendData));
+  });
+
+  it("should get all friends", async () => {
+    prisma.friend.findMany = jest.fn().mockResolvedValue([
+      {
+        id: 1,
+        firstName: "John",
+        lastName: "Doe",
+        phoneNumber: "1234567890",
+        email: "john.doe@email.com",
+        notes: "This is John Doe.",
+      },
+      {
+        id: 2,
+        firstName: "Jane",
+        lastName: "Doe",
+        phoneNumber: "0987654321",
+        email: "jane.doe@email.com",
+        notes: "This is Jane Doe.",
+      },
+    ]);
+
+    const friends = await getAllFriends();
+    expect(friends).toEqual([
+      expect.objectContaining({
+        id: 1,
+        firstName: "John",
+        lastName: "Doe",
+        phoneNumber: "1234567890",
+        email: "john.doe@email.com",
+        notes: "This is John Doe.",
+      }),
+      expect.objectContaining({
+        id: 2,
+        firstName: "Jane",
+        lastName: "Doe",
+        phoneNumber: "0987654321",
+        email: "jane.doe@email.com",
+        notes: "This is Jane Doe.",
+      }),
+    ]);
   });
 });
