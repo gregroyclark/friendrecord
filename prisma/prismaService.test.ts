9144127ea2c8fff2368a336353547f5ee0e6c6a6
gregroyclark <gregroyclark@gmail.com>
2023-12-20 19:03:32 -0800

    breaks up test suites by service adds comment blocks for readability

--- prisma/prismaService.test.ts
+++ prisma/prismaService.test.ts
@@ -13,106 +13,158 @@ import {
 
 import prisma from "./prisma";
 
+/*
+    ==========================================
+
+    test suites for prismaService.ts
+
+    ==========================================
+
+  */
 void describe("prismaService", async () => {
-  it("should create a friend", async () => {
-    const friendData = {
-      firstName: "Greg",
-      lastName: "Clark",
-      phoneNumber: "1234567890",
-      email: "test@email.com",
-      notes: "This is a jest unit test",
-    };
-    const newFriend = await createFriend(friendData);
-    expect(newFriend).toEqual(expect.objectContaining(friendData));
+  /*
+    ==========================================
+
+    test suites for createFriend prismaService
+
+    ==========================================
+
+  */
+
+  void describe("createFriend", async () => {
+    it("should create a friend", async () => {
+      const friendData = {
+        firstName: "Greg",
+        lastName: "Clark",
+        phoneNumber: "1234567890",
+        email: "test@email.com",
+        notes: "This is a jest unit test",
+      };
+      const newFriend = await createFriend(friendData);
+      expect(newFriend).toEqual(expect.objectContaining(friendData));
+    });
   });
 
-  it("should get all friends", async () => {
-    prisma.friend.findMany = jest.fn().mockResolvedValue([
-      {
+  /*
+    ==========================================
+
+    test suites for getAllFriends prismaService
+
+    ==========================================
+
+  */
+
+  void describe("getAllFriends", () => {
+    it("should get all friends", async () => {
+      prisma.friend.findMany = jest.fn().mockResolvedValue([
+        {
+          id: 1,
+          firstName: "John",
+          lastName: "Doe",
+          phoneNumber: "1234567890",
+          email: "john.doe@email.com",
+          notes: "This is John Doe.",
+        },
+        {
+          id: 2,
+          firstName: "Jane",
+          lastName: "Doe",
+          phoneNumber: "0987654321",
+          email: "jane.doe@email.com",
+          notes: "This is Jane Doe.",
+        },
+      ]);
+
+      const friends = await getAllFriends();
+      expect(friends).toEqual([
+        expect.objectContaining({
+          id: 1,
+          firstName: "John",
+          lastName: "Doe",
+          phoneNumber: "1234567890",
+          email: "john.doe@email.com",
+          notes: "This is John Doe.",
+        }),
+        expect.objectContaining({
+          id: 2,
+          firstName: "Jane",
+          lastName: "Doe",
+          phoneNumber: "0987654321",
+          email: "jane.doe@email.com",
+          notes: "This is Jane Doe.",
+        }),
+      ]);
+    });
+  });
+
+  /*
+    ==========================================
+
+    test suites for getFriend prismaService
+
+    ==========================================
+
+  */
+
+  void describe("getFriend", async () => {
+    it("should get a friend", async () => {
+      // mock the prisma.friend.findUniqueOrThrow function
+      prisma.friend.findUniqueOrThrow = jest.fn().mockResolvedValue({
         id: 1,
         firstName: "John",
         lastName: "Doe",
         phoneNumber: "1234567890",
         email: "john.doe@email.com",
         notes: "This is John Doe.",
-      },
-      {
-        id: 2,
-        firstName: "Jane",
-        lastName: "Doe",
-        phoneNumber: "0987654321",
-        email: "jane.doe@email.com",
-        notes: "This is Jane Doe.",
-      },
-    ]);
+      });
 
-    const friends = await getAllFriends();
-    expect(friends).toEqual([
-      expect.objectContaining({
-        id: 1,
-        firstName: "John",
-        lastName: "Doe",
-        phoneNumber: "1234567890",
-        email: "john.doe@email.com",
-        notes: "This is John Doe.",
-      }),
-      expect.objectContaining({
-        id: 2,
-        firstName: "Jane",
-        lastName: "Doe",
-        phoneNumber: "0987654321",
-        email: "jane.doe@email.com",
-        notes: "This is Jane Doe.",
-      }),
-    ]);
-  });
-
-  it("should get a friend", async () => {
-    // mock the prisma.friend.findUniqueOrThrow function
-    prisma.friend.findUniqueOrThrow = jest.fn().mockResolvedValue({
-      id: 1,
-      firstName: "John",
-      lastName: "Doe",
-      phoneNumber: "1234567890",
-      email: "john.doe@email.com",
-      notes: "This is John Doe.",
+      const friend = await getFriend(1);
+      expect(friend).toEqual(
+        expect.objectContaining({
+          id: 1,
+          firstName: "John",
+          lastName: "Doe",
+          phoneNumber: "1234567890",
+          email: "john.doe@email.com",
+          notes: "This is John Doe.",
+        }),
+      );
     });
 
-    const friend = await getFriend(1);
-    expect(friend).toEqual(
-      expect.objectContaining({
-        id: 1,
-        firstName: "John",
-        lastName: "Doe",
-        phoneNumber: "1234567890",
-        email: "john.doe@email.com",
-        notes: "This is John Doe.",
-      }),
-    );
-  });
+    // edge case for when query is null
+    it("should return null when friend does not exist", async () => {
+      prisma.friend.findUniqueOrThrow = jest.fn().mockResolvedValue(null);
+      const friend = await getFriend(1);
+      expect(friend).toBeNull();
+    });
 
-  it("should return null when friend does not exist", async () => {
-    prisma.friend.findUniqueOrThrow = jest.fn().mockResolvedValue(null);
-    const friend = await getFriend(1);
-    expect(friend).toBeNull();
-  });
+    // test of edge case when id is NaN
+    it("should return an error when id is not a number", async () => {
+      try {
+        await getFriend("invalidId");
+      } catch (error) {
+        expect(error).toBeInstanceOf(error);
+      }
+    });
 
-  it("should return an error when id is not a number", async () => {
+    // test of edge case, database error
+    it("should return an error when there is a database error", async () => {
+      prisma.friend.findUniqueOrThrow = jest
+        .fn()
+        .mockResolvedValue(new Error("Database error"));
+    });
     try {
-      await getFriend("invalidId");
+      await getFriend(1);
     } catch (error) {
-      expect(error).toBeInstanceOf(error);
+      expect(error).toBeInstanceOf(Error);
     }
   });
+  /*
+    ==========================================
 
-  it("should return an error when there is a database error", async () => {
-    prisma.friend.findUniqueOrThrow = jest
-      .fn()
-      .mockResolvedValue(new Error("Database error"));
-  });
-  try {
-    await getFriend(1);
-  } catch (error) {
-    expect(error).toBeInstanceOf(Error);
-  }
+    test suites for updateFriend prismaService
+
+    ==========================================
+
+  */
 });
