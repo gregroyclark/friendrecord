e61801ef315029b42631298b3291fd7949a3b280
gregroyclark <gregroyclark@gmail.com>
2023-12-22 23:05:33 -0800

    only run this test on local db

--- prisma/prisma.test.ts
+++ prisma/prisma.test.ts
@@ -1,22 +1,24 @@
-import prisma from "./prisma";
+// import prisma from "./prisma";
 
-/*
-    ==========================================
+// can only run this test on local db for now
 
-    test suite for prisma.ts
+// /*
+//     ==========================================
 
-    ==========================================
+//     test suite for prisma.ts
 
-  */
+//     ==========================================
 
-describe("Prisma Client", () => {
-  it("should initialize the Prisma Client", async () => {
-    // check if the prisma client is defined
-    expect(prisma).toBeDefined();
+//   */
 
-    // try to connect to the database
-    await prisma.$connect();
+// describe("Prisma Client", () => {
+//   it("should initialize the Prisma Client", async () => {
+//     // check if the prisma client is defined
+//     expect(prisma).toBeDefined();
 
-    // if the connection is successful, the test will pass
-  });
-});
+//     // try to connect to the database
+//     await prisma.$connect();
+
+//     // if the connection is successful, the test will pass
+//   });
+// });
