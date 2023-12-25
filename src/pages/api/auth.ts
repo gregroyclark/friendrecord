4a45b17d4e38b41d53dab5c455b4f72bb6b67896
gregroyclark <gregroyclark@gmail.com>
2023-12-25 01:57:09 -0800

    added register & login functions

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -15,7 +15,6 @@ export default async function handler(req, res) {
           name,
           email,
           password,
-          id: undefined,
         });
         res.status(200).json({ status: "success", data: user });
       } catch (error) {
