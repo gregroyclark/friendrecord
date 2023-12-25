2f92e322054d7d34c185edf2f2e881fdb8c0d9b8
gregroyclark <gregroyclark@gmail.com>
2023-12-25 02:07:48 -0800

    updated SignUp function

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,23 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { useState } from "react";
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
+import { login } from "prisma/prismaService";
 
 const SignUpPage = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await fetch("/api/createUser", {
+    const response = await fetch("/api/register", {
       method: "POST",
-      body: JSON.stringify({ name, email }),
+      body: JSON.stringify({ name, email, password }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      void signIn("google");
+      void login(email, password);
     } else {
     }
   };
@@ -36,6 +39,13 @@ const SignUpPage = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
         required
       />
       <button type="submit">Sign Up</button>
