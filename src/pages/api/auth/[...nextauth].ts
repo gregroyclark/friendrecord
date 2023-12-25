dc684a1c5b4a587b162ce72dc77765a3c4e21484
gregroyclark <gregroyclark@gmail.com>
2023-12-24 20:39:36 -0800

    added google provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,15 @@
-// /* eslint-disable @typescript-eslint/no-unsafe-argument */
-// import NextAuth from "next-auth";
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-// import { authOptions } from "~/server/auth";
-
-// export default NextAuth(authOptions);
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
