ac075a8175ef34642616f7c9d63700cbcc9b0af4
gregroyclark <gregroyclark@gmail.com>
2023-12-29 10:44:54 -0800

    updated style and structure, added links

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,6 +4,7 @@
 import { useState } from "react";
 // import { signIn } from "next-auth/react";
 import { login } from "prisma/prismaService";
+import Link from "next/link";
 
 const SignUpPage = () => {
   const [email, setEmail] = useState("");
@@ -26,30 +27,62 @@ const SignUpPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Name"
-        required
-      />
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className="flex flex-col items-center justify-center space-y-4 bg-[#e4e2DD] p-8 shadow-md">
+      <h1 className="m-4 flex justify-center text-lg font-semibold text-gray-600">
+        Welcome to friendrecord!
+      </h1>
+      <div className="w-full">
+        <hr />
+      </div>
+      <div className="flex w-full items-center justify-center">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+            required
+            className="rounded-md border border-gray-300 bg-gray-50 p-2"
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+            className="rounded-md border border-gray-300 bg-gray-50 p-2"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+            className="rounded-md border border-gray-300 bg-gray-50 p-2"
+          />
+          <button
+            type="submit"
+            className="bg--blue-700 m-1 rounded-md bg-green-700 p-1.5 text-white hover:bg-green-800"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+      {/* <button>Sign up with Google</button> */}
+      <div className="">
+        <div className="w-full">
+          <hr />
+        </div>
+        <div>
+          <p className="m-4 flex justify-center text-lg font-semibold text-gray-600">
+            Already have an account?
+            <span>
+              <Link href={"/Login"}>Login</Link>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
