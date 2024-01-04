8a1ce388de44cb56d735c4fdca86b932975657a2
gregroyclark <gregroyclark@gmail.com>
2024-01-03 21:11:12 -0800

    fixed login flow

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-import { signIn } from "next-auth/react";
 import { type NextApiRequest, type NextApiResponse } from "next";
 
-// import { login } from "prisma/prismaService";
+import { login } from "prisma/prismaService";
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,45 +17,15 @@ export default async function handler(
 
     console.log("Attempting to sign in with email: ", email);
 
-    if (typeof window !== "undefined") {
-      try {
-        await signIn("credentials", {
-          email: email,
-          password: password,
-          redirect: false,
-        });
-
-        console.log("Successfully signed in");
-
-        res.status(200).json({ status: "Login successful" });
-      } catch (error) {
-        console.error("Error signing in: ", error);
-        res.status(500).json({ error: error });
-      }
-    } else {
-      res
-        .status(400)
-        .json({ error: "Sign in must be performed on the client-side" });
+    try {
+      const user = await login(email, password);
+      res.status(200).json(user);
+      console.log("Successfully signed in");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error });
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
 }
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse,
-// ) {
-//   if (req.method === "POST") {
-//     const { email, password } = req.body;
-//     try {
-//       const { user } = await login(email, password);
-//       res.status(200).json({ user });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: error });
-//     }
-//   } else {
-//     res.status(405).json({ error: "Method not allowed" });
-//   }
-// }
