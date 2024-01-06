0780208fe51451561b7603824ebb09739958a5a5
gregroyclark <gregroyclark@gmail.com>
2024-01-05 17:10:51 -0800

    db setup

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.MYSQL_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database');
+});
+
+module.exports = { db };
