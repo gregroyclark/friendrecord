88f43dd35fcf77f8633da5b75beea1dd00a7b2bf
gregroyclark <gregroyclark@gmail.com>
2024-01-06 04:52:04 -0800

    added bodyparser & cookieparser moved auth code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const path = require('path');
@@ -9,29 +10,34 @@ const routes = require('./routes');
 
 const app = express();
 
-// auth goes here
-// function authenticateToken(req, res, next) {
-//   const token = req.cookies.jwt;
-//   if (token == null)
-//     return res.status(401).send({ message: 'No token provided' });
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-//     if (err)
-//       return res.status(403).send({ message: 'Failed to authenticate token' });
-//     req.user = user;
-//     next();
-//   });
-// }
-// module.exports.authenticateToken = authenticateToken;
-
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(logger('combined'));
 app.use(express.json());
 app.use(cors());
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// auth goes here
+function authenticateToken(req, res, next) {
+  const token = req.cookies.jwt;
+  if (token == null)
+    return res.status(401).send({ message: 'No token provided' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err)
+      return res.status(403).send({ message: 'Failed to authenticate token' });
+    req.user = user;
+    next();
+  });
+}
+
+app.use(authenticateToken);
+// module.exports.authenticateToken = authenticateToken;
 
 // app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
