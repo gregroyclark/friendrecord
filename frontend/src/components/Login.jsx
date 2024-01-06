249f5c389829eb796765ec914035a495a92cb83d
gregroyclark <gregroyclark@gmail.com>
2024-01-05 22:15:17 -0800

    added component stubs, auth

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const Login = () => {
+  // state
+  // state
+
+  const handleSubmit = async((event) => {
+    // api call
+  });
+
+  return (
+    <div>
+      <p>log in</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/src/components/SignUp.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const SignUp = () => {
+  // state
+  // state
+
+  const handleSubmit = async((event) => {
+    // api call
+  });
+
+  return (
+    <div>
+      <p>sign up</p>
+    </div>
+  );
+};
+
+export default SignUp;
