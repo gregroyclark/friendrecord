fb595e064e6a4bda909083dd2e8d50a3e029df1c
gregroyclark <gregroyclark@gmail.com>
2024-01-05 22:05:09 -0800

    added Router added HOC component, AuthenticatedRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
+
+import AuthenticatedRoute from './components/AuthenticatedRoute';
 
 import Navbar from './components/Navbar';
 import AddFriend from './components/AddFriend';
+// import FriendList from './components/FriendList';
+// import UpdateFriend from './components/UpdateFriend'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <AddFriend />
-    </>
+      <Switch>
+        <AuthenticatedRoute exact path='/' component={AddFriend} />
+        {/* <AuthenticatedRoute exact path='/' component={FriendList} /> */}
+        {/* <AuthenticatedRoute exact path='/' component={UpdateFriend} /> */}
+      </Switch>
+    </Router>
   );
 }
 
