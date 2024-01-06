c76c51a1bec73a2280d01919c979a65675780f17
gregroyclark <gregroyclark@gmail.com>
2024-01-06 00:49:52 -0800

    added log messages to auth block

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,12 @@ const app = express();
 // auth goes here
 function authenticateToken(req, res, next) {
   const token = req.cookies.jwt;
-  if (token == null) return res.status(401);
+  if (token == null)
+    return res.status(401).send({ message: 'No token provided' });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err)
+      return res.status(403).send({ message: 'Failed to authenticate token' });
     req.user = user;
     next();
   });
@@ -33,6 +35,7 @@ app.set('view engine', 'pug');
 app.use(routes);
 
 module.exports.authenticateToken = authenticateToken;
+
 app.listen(port, () =>
   console.log(`🌎 ==> API Server now listening on port ${port}`)
 );
