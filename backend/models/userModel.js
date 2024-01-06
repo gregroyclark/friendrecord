f1adf33b487ec9bf9a66914f8b93ca672bf9e18d
gregroyclark <gregroyclark@gmail.com>
2024-01-06 04:11:02 -0800

    updated model, createUser - values - query - uuid updated db import

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,13 @@
-const db = require('../config/database');
+const { v4: uuidv4 } = require('uuid');
 
-exports.createUser = (email, password, role) => {
+const { db } = require('../config/database');
+
+exports.createUser = (firstName, lastName, email, hashedPassword) => {
   return new Promise((resolve, reject) => {
-    const query = 'INSERT INTO users SET ?';
-    const values = { email, password, role };
+    const userId = uuidv4();
+    const query =
+      'INSERT INTO users (firstName, lastName, email, hashedPassword, userId) VALUES (?, ?, ?, ?, ?)';
+    const values = [firstName, lastName, email, hashedPassword, userId];
     db.query(query, values, (err, result) => {
       if (err) reject(err);
       resolve(result);
