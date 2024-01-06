9aaeef553abedb60c572ffd8155f1480418a0c97
gregroyclark <gregroyclark@gmail.com>
2024-01-05 22:23:54 -0800

    added component, SignUp

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,17 +1,49 @@
 import { useState } from 'react';
 
 const SignUp = () => {
-  // state
-  // state
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = async((event) => {
-    // api call
-  });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5000/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error('HTTP error: ' + response.status);
+      }
+
+      const data = await response.json();
+      console.log('Success: ', data);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
+  };
 
   return (
-    <div>
-      <p>sign up</p>
-    </div>
+    <form>
+      <label>
+        Email:
+        <input
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type='submit'>Sign Up</button>
+    </form>
   );
 };
 
