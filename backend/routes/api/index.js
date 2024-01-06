472d3862495c06bb8d7dc5253f077bbb01b8a795
gregroyclark <gregroyclark@gmail.com>
2024-01-05 21:35:16 -0800

    added api route, users

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
 const friendRoutes = require('./friends');
+const userRoutes = require('./users');
 
 // Friend routes
 router.use('/friends', friendRoutes);
+
+// User routes
+router.use('/users', userRoutes);
 
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, '../../../frontend/build/index.html'));
