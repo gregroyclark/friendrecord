19251fa1d996d074a0da3185f16dc62a5b6b6daa
gregroyclark <gregroyclark@gmail.com>
2024-01-06 05:00:38 -0800

    added api authorization changed element into a table

--- frontend/src/components/FriendList.jsx
+++ frontend/src/components/FriendList.jsx
@@ -1,34 +1,57 @@
 import { useEffect, useState } from 'react';
 
 const FriendList = ({ userId }) => {
-  const [friends, setFriends] = useState([]);
+  const [friends, setFriends] = useState(null);
 
   useEffect(() => {
-    const fetchFriends = async () => {
+    const readFriends = async () => {
       try {
-        const response = await fetch(`/friends/${userId}`);
+        const response = await fetch(
+          `http://localhost:5000/api/friends/${userId}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        // const text = await response.text();
+        // console.log(text);
         const data = await response.json();
+        // console.log(data);
         setFriends(data);
       } catch (error) {
         console.error('Error reading all friends: ', error);
       }
     };
 
-    fetchFriends();
-  });
+    readFriends();
+  }, [userId]);
+
+  if (friends === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      {friends.map((friend) => (
-        <div key={friend.id}>
-          <h2>
-            {friend.firstName} {friend.lastName}
-          </h2>
-          <p>{friend.email}</p>
-          <p>{friend.phoneNumber}</p>
-          <p>{friend.notes}</p>
-        </div>
-      ))}
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Notes</th>
+        </tr>
+      </thead>
+      <tbody>
+        {friends.map((friend) => (
+          <tr key={friend.id}>
+            <td>{friend.firstName}</td>
+            <td>{friend.lastName}</td>
+            <td>{friend.email}</td>
+            <td>{friend.phoneNumber}</td>
+            <td>{friend.notes}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
