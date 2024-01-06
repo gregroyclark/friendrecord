aad1274d7baeeb403e5e9c6bc5fee156df5f0c4a
gregroyclark <gregroyclark@gmail.com>
2024-01-06 01:50:19 -0800

    refactored into modules

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,13 +2,24 @@ const db = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-exports.register = async (req, res) => {
+/* 
+
+  ==========================================
+
+  Handles incoming HTTP requests
+  Calls corresponding functions in the model
+
+  ==========================================
+
+*/
+
+const register = async (req, res) => {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
   const user = await db.createUser(req.body.email, hashedPassword, 'user');
   res.status(201).send(user);
 };
 
-exports.login = async (req, res) => {
+const login = async (req, res) => {
   const user = await db.findUserByEmail(req.body.email);
   if (user && (await bcrypt.compare(req.body.password, user.password))) {
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
@@ -20,3 +31,5 @@ exports.login = async (req, res) => {
     res.status(401).send('Invalid username or password');
   }
 };
+
+module.exports = { register, login };
