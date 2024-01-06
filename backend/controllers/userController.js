235ddf8cbc842702fc0b38a5689c699401b45958
gregroyclark <gregroyclark@gmail.com>
2024-01-06 05:02:37 -0800

    added logging updated auth flow

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,20 +22,49 @@ const register = async (req, res) => {
     hashedPassword,
     'user'
   );
+  console.log('User registered: ', user);
   res.status(201).send(user);
 };
 
 const login = async (req, res) => {
+  console.log('Handling login request...');
+
   const user = await db.findUserByEmail(req.body.email);
-  if (user && (await bcrypt.compare(req.body.password, user.password))) {
+  console.log('User found: ', user);
+
+  let isAuthenticated = false;
+
+  if (user) {
+    isAuthenticated = await bcrypt.compare(
+      req.body.password,
+      user.hashedPassword
+    );
+    console.log('Authentication result: ', isAuthenticated);
+  }
+
+  if (isAuthenticated) {
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
+    console.log('Generated JWT: ', token);
+
     res.cookie('jwt', token, { httpOnly: true });
     res.status(200).send(user);
   } else {
+    console.log('Authentication failed');
     res.status(401).send('Invalid username or password');
   }
+
+  // if (user && (await bcrypt.compare(req.body.password, user.password))) {
+  //   const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+  //     expiresIn: '1h',
+  //   });
+
+  //   res.cookie('jwt', token, { httpOnly: true });
+  //   res.status(200).send(user);
+  // } else {
+  //   res.status(401).send('Invalid username or password');
+  // }
 };
 
 module.exports = { register, login };
