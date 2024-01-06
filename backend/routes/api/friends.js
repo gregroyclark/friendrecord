90c02f37699a9235214a88f9616ca4ab105ab7e0
gregroyclark <gregroyclark@gmail.com>
2024-01-05 22:15:43 -0800

    added jwt authentication

--- backend/routes/api/friends.js
+++ backend/routes/api/friends.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateToken } = require('../../server');
 const friendController = require('../../controllers/friendController');
 
 router.get('/', (req, res) => {
   res.send('GET request to the homepage');
 });
-router.post('/', friendController.createFriend);
-router.get('/:userId', friendController.readAllFriends);
-router.get('/:id', friendController.readOneFriend);
-router.put('/:id', friendController.updateFriend);
-router.delete('/:id', friendController.deleteFriend);
+router.post('/', authenticateToken, friendController.createFriend);
+router.get('/:userId', authenticateToken, friendController.readAllFriends);
+router.get('/:id', authenticateToken, friendController.readOneFriend);
+router.put('/:id', authenticateToken, friendController.updateFriend);
+router.delete('/:id', authenticateToken, friendController.deleteFriend);
 
 module.exports = router;
