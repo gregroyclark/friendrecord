a4da4de7145e460df46b3688e4854355504dd314
gregroyclark <gregroyclark@gmail.com>
2024-01-05 22:15:32 -0800

    added jwt authentication

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,23 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 
 const routes = require('./routes');
 
 const app = express();
+
+// auth goes here
+function authenticateToken(req, res, next) {
+  const token = req.cookies.jwt;
+  if (token == null) return res.status(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 const port = process.env.PORT || 5000;
 
@@ -19,6 +32,7 @@ app.set('view engine', 'pug');
 
 app.use(routes);
 
+module.exports.authenticateToken = authenticateToken;
 app.listen(port, () =>
   console.log(`🌎 ==> API Server now listening on port ${port}`)
 );
