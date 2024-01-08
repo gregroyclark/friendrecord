32c9119c45f8bb6bc1b2cdbca0bcce144b368b65
gregroyclark <gregroyclark@gmail.com>
2024-01-08 01:36:57 -0800

    fixed api routes

--- backend/routes/api/friends.js
+++ backend/routes/api/friends.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const authenticateToken = require('../../server').authenticateToken;
+
 const {
   createFriend,
   readAllFriends,
@@ -8,19 +8,19 @@ const {
   updateFriend,
   deleteFriend,
 } = require('../../controllers/friendController');
+// const authenticateToken = require('../../server').authenticateToken;
 
-router.get('/', (req, res) => {
-  res.send('GET request to the homepage');
-});
-router.post('/', createFriend);
-router.get('/:userId', readAllFriends);
-router.get('/:id', readOneFriend);
-router.put('/:id', updateFriend);
-router.delete('/:id', deleteFriend);
-// router.post('/', authenticateToken, createFriend);
-// router.get('/:userId', authenticateToken, readAllFriends);
-// router.get('/:id', authenticateToken, readOneFriend);
-// router.put('/:id', authenticateToken, updateFriend);
-// router.delete('/:id', authenticateToken, deleteFriend);
+router.post('/createFriend', createFriend);
+router.get('/readAllFriends/:userId', readAllFriends);
+router.get('/readOneFriend/:id', readOneFriend);
+router.put('/updateFriend/:id', updateFriend);
+router.delete('/deleteFriend/:id', deleteFriend);
+
+// // attempting to authenticate with jwt
+// router.post('/createFriend/', authenticateToken, createFriend);
+// router.get('/readAllFriends/:userId', authenticateToken, readAllFriends);
+// router.get('/readOneFriend/:id', authenticateToken, readOneFriend);
+// router.put('/updateFriend/:id', authenticateToken, updateFriend);
+// router.delete('/deleteFriend/:id', authenticateToken, deleteFriend);
 
 module.exports = router;
