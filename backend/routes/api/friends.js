9d36b2d4a37a91fe3cd608d86357f72cb61efe6e
gregroyclark <gregroyclark@gmail.com>
2024-01-13 23:22:32 -0800

    removed dead comments

--- backend/routes/api/friends.js
+++ backend/routes/api/friends.js
@@ -8,19 +8,11 @@ const {
   updateFriend,
   deleteFriend,
 } = require('../../controllers/friendController');
-// const authenticateToken = require('../../server').authenticateToken;
 
 router.post('/createFriend', createFriend);
 router.get('/readAllFriends/:userId', readAllFriends);
 router.get('/readOneFriend/:id', readOneFriend);
 router.put('/updateFriend/:id', updateFriend);
 router.delete('/deleteFriend/:id', deleteFriend);
-
-// // attempting to authenticate with jwt
-// router.post('/createFriend/', authenticateToken, createFriend);
-// router.get('/readAllFriends/:userId', authenticateToken, readAllFriends);
-// router.get('/readOneFriend/:id', authenticateToken, readOneFriend);
-// router.put('/updateFriend/:id', authenticateToken, updateFriend);
-// router.delete('/deleteFriend/:id', authenticateToken, deleteFriend);
 
 module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,3 @@
-// const path = require('path');
 const router = require('express').Router();
 const friendRoutes = require('./friends');
 const userRoutes = require('./users');
@@ -8,9 +7,5 @@ router.use('/friends', friendRoutes);
 
 // User routes
 router.use('/users', userRoutes);
-
-// router.use(function (req, res) {
-//   res.sendFile(path.join(__dirname, '../../../frontend/', 'index.html'));
-// });
 
 module.exports = router;
